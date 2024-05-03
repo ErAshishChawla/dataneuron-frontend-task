96fc15ef34acd972c290d986a3fbe0e88907cda2
ErAshishChawla <ashishchawladev@gmail.com>
2024-05-03 08:05:02 -0400

    Completed the frontend part for deployment

--- lib/api/api-instance.ts
+++ lib/api/api-instance.ts
@@ -2,5 +2,7 @@ import axios from "axios";
 
 // Create an axios instance to use for all client API requests
 export const api = axios.create({
-  baseURL: process.env.SERVER_URL! || "http://localhost:8000/api",
+  baseURL:
+    process.env.SERVER_URL! ||
+    "https://dataneuron-backend-task.onrender.com/api",
 });
--- lib/sc-fetcher.ts
+++ lib/sc-fetcher.ts
@@ -2,7 +2,9 @@
 
 // Fetcher for server-client communication. Axios cant be used in server components without adapter, but fetch can be used in both client and server.
 export async function scFetcher(url: string, method?: string, body?: any) {
-  const serverURL = process.env.SERVER_URL || "http://localhost:8000/api";
+  const serverURL =
+    process.env.SERVER_URL ||
+    "https://dataneuron-backend-task.onrender.com/api";
   const requestURL = `${serverURL}${url}`;
 
   const response = await fetch(requestURL, {
